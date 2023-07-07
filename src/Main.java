188e19d0dd7f43573d50e19cf5f3562df0bc0cfe
nikola <djordjevic.nikola.it@gmail.com>
2023-07-07 19:37:39 +0200

    Updated multiArray

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         String [][] x ={
                 {"računar","tablet","mobilni telefon"},
                 {"vanila","so","biber","cimet"},
-                {"kvaka", "stolnjak"}
+                {"kvaka", "stolnjak","radkapna"}
         };
         for(int i=0;i<x.length;i++){
             System.out.println("  Pojmovi:  ");
